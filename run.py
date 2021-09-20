eb184532950066918bc8673d907c8b66da7d4b09
Rimmy Aulakh <rimmyaulakh@Rimmys-MacBook-Pro.local>
2021-09-20 15:23:37 -0400

    First Commit

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from datetime import datetime, timedelta
+app = Flask(__name__)
+
+@app.route('/time')
+def hello_world():
+    today = datetime.now() - timedelta(hours=4, minutes=0)
+    return today.strftime("%H:%M")
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
